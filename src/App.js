c8c97468bf1fc0998091607db79f0e6995ca6e5a
Bryan <player.b.96@hotmail.com>
2021-09-21 20:44:36 -0500

    se agrega vista de registro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Login from './components/login';
+import Login from './components/login/login';
+import Registro from './components/registro/registro';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Registro />
     </div>
   );
 }
---
+++ src/components/registro/registro.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './registro.css'
+
+export default function Registro(){
+    return (
+        <body>
+            <div className="loginContainer">
+                <h1> Registro </h1>
+                <div className= "backgroundlogin">
+                    <input className= "backInput" type="text" placeholder="Nombres"></input>
+                    <input className= "backInput" type="text" placeholder="Apellidos"></input>
+                    <input className= "backInput" type="email" placeholder="Email"></input>
+                    <input className= "backInput" type="password" placeholder="Contraseña"></input>
+                    <input className= "backInput" type="password" placeholder="Confirmar Contraseña"></input>
+                    <div className ="contenedorBotones">
+                        <a className = "botonBack"> Back </a> <a className = "botonlogin"> Registrar </a>
+                    </div>
+                </div>
+                
+            </div>
+        </body>
+    );
+}
