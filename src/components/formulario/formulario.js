aab5a29e8018bea677c0bfab55011ddfcf851cef
Bryan <player.b.96@hotmail.com>
2021-09-27 17:40:29 -0500

    se añade detalles de preguntas y diseño

--- src/components/formulario/formulario.js
+++ src/components/formulario/formulario.js
@@ -20,13 +20,59 @@ export default function Formulario(){
                     <div className="cargoFechaContainer">
                         <div className="cargoContainer">
                             <h2 className="h2container">Cargo</h2>
-                            <input className="inputcontainerdate" type="text"  placeholder = "Apellido 1"></input>
+                            <input className="inputcontainerdate" type="text"   ></input>
                         </div>
                         <div className="fechaContainer">
                             <h2 className="h2container">Fecha</h2>
-                            <input className="inputcontainerdate" type="date"  placeholder = "Apellido 1"></input>
+                            <input className="inputcontainerdate" type="date" ></input>
                         </div>
                     </div>
+
+                    <div>
+                        <h1 className="valoraciones">Valoraciones</h1>
+                        <h1 className="calificaciones">Insuficiente (1) - Regular (2) - Bueno (3) - Muy Bueno (4) - Excelente (5)</h1>
+                    </div>
+
+                    <div className="fondoquests">
+
+                    <div className="crecimientoydesarrollo">
+                        <h1 className="fontsize"> Crecimiento y Desarrollo </h1>
+                    </div>
+
+                        <div className="grindquests">
+                            <div>
+                                <h1></h1>
+                            </div>
+                            <div className="contentvaloraciones">
+                                <h1 className="valoracionesh1">VALORACIONES</h1>
+                            </div>
+
+                            <div>
+                                <h1 className="h1conteint">¿Está satisfecho con las oportunidades de crecimiento de tu carrera en esta empresa, y hay un plan claro disponible para hacerlo?</h1>
+                            </div>
+                            <div className="conteintdesplegables">
+                                <h1></h1>
+                            </div>
+
+                            <div>
+                                <h1 className="h1conteint">¿Tu gerente te comunica las expectativas y te da retroalimentación de manera clara y profesional?</h1>
+                            </div>
+                            <div className="conteintdesplegables">
+                                <h1></h1>
+                            </div>
+
+                            <div>
+                                <h1 className="h1conteint">¿Tienes todas las herramientas necesarias para hacer tu mejor trabajo?</h1>
+                            </div>
+                            <div className="conteintdesplegables">
+                                <h1></h1>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+
+                    
                 </div>
             </div>
         </body>
